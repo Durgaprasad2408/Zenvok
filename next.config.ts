8015fd2ce39057662241809825a4bbc2733612ec
Durgaprasad2408 <durgaprasad.p@outlook.in>
2026-01-03 18:08:59 +0530

    error fixing

--- next.config.ts
+++ next.config.ts
@@ -17,19 +17,6 @@ const nextConfig: NextConfig = {
     ],
   },
   outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
 };
 
 export default nextConfig;
